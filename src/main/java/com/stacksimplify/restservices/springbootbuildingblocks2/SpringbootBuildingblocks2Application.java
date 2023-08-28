4f386ea2a48b8e62759d4c5b0d2a42192246caed
Gomez Martinez <sagomezmartinez@deloitte.com>
2023-08-28 15:09:50 -0600

    Hello World Base

--- src/main/java/com/stacksimplify/restservices/springbootbuildingblocks2/SpringbootBuildingblocks2Application.java
+++ src/main/java/com/stacksimplify/restservices/springbootbuildingblocks2/SpringbootBuildingblocks2Application.java
@@ -2,8 +2,10 @@ package com.stacksimplify.restservices.springbootbuildingblocks2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 //Second Commit
 @SpringBootApplication
+@ComponentScan(basePackages = "com.stacksimplify.restservices.Hello")
 public class SpringbootBuildingblocks2Application {
 
 	public static void main(String[] args) {
