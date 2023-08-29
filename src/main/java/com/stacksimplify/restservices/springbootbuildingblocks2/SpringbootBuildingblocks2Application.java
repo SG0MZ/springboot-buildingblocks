866ee924f35e93f806f7716d2d01f108c1fe42b3
sagomezmartinez <sagomezmartinez@USQROSAGOMEZMA1.us.deloitte.com>
2023-08-29 12:01:41 -0500

    JPA Base RestServices Development Completed

--- src/main/java/com/stacksimplify/restservices/springbootbuildingblocks2/SpringbootBuildingblocks2Application.java
+++ src/main/java/com/stacksimplify/restservices/springbootbuildingblocks2/SpringbootBuildingblocks2Application.java
@@ -2,10 +2,17 @@ package com.stacksimplify.restservices.springbootbuildingblocks2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 //Second Commit
 @SpringBootApplication
-@ComponentScan(basePackages = "com.stacksimplify.restservices.Hello")
+@ComponentScan(basePackages = {"com.stacksimplify.restservices.Hello"
+		,"com.stacksimplify.restservices.repositories"
+		,"com.stacksimplify.restservices.controllers"
+		,"com.stacksimplify.restservices.services"})
+@EntityScan("com.stacksimplify.restservices.entities")
+@EnableJpaRepositories("com.stacksimplify.restservices.repositories")
 public class SpringbootBuildingblocks2Application {
 
 	public static void main(String[] args) {
